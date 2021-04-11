7943b3f21db41dd42ce70a138db3b70aa87c3a5f
molibdy <molipotts@gmail.com>
2021-04-11 21:49:58 +0200

    subida

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/microcareApp'));
 app.get('/*',function(req,res){
-    res.sendFile("index.html", {root:__dirname + '/dist/microcareApp/'});
+    res.sendFile(path.join(__dirname + '/dist/microcareApp/index.html'));
 });
 
 app.listen(process.env.PORT  || 8080);
