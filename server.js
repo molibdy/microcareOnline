8d6a1b00a1322274fd657f7145168594990935ff
molibdy <molipotts@gmail.com>
2021-04-13 22:58:24 +0200

    prueba server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/microcareApp'));
-app.get('*',function(req,res){
+app.get('/*',function(req,res){
     // res.sendFile("index.html", {root:__dirname + '/dist/microcareApp/'});
-    res.sendFile(path.join(__dirname + '/dist/microcareApp/index.html'));
+    
+    res.sendFile(path.join(__dirname + '/dist/microcare-app/index.html'));
+    // res.sendFile(path.join(__dirname + '/dist/microcareApp/index.html'));
 });
 
 app.listen(process.env.PORT  || 8080);
