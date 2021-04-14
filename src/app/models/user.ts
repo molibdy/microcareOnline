2ce6e4577ce9f0bd6060c2f490f8e19dc379bd69
rmoliner17 <rodrigomoliner@gmail.com>
2021-04-14 13:37:46 +0200

    servicio login

---
+++ src/app/models/user.ts
@@ -0,0 +1,20 @@
+
+
+export class User {
+
+    public user_id :number = 0;
+    public username :string = "";
+    public password :string = "";
+    public email: string = "";
+    public profile_picture: string="";
+
+    constructor(user_id:number, username:string, password:string, email:string)
+    {
+    this.user_id = user_id
+    this.username = username
+    this.password = password
+    this.email = email
+
+    }
+
+}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { LoginInfoService } from 'src/app/shared/login-info.service';
+
 
 @Component({
   selector: 'app-register',
@@ -9,26 +12,56 @@ import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/for
 
 export class RegisterComponent implements OnInit {
   public myForm: FormGroup
-  constructor(private formBuilder: FormBuilder) { 
+
+  public respuestaPositiva:string
+  public respuestaNegativa:string
+  
+  constructor(private formBuilder: FormBuilder, private apiService: LoginInfoService) { 
     this.buildForm()
+    this.respuestaPositiva = ""
+    this.respuestaNegativa =""
   }
  
 
   ngOnInit(): void {
   }
 
-  private buildForm(){
-
-    const minPasswordLength = 8
-    
-    this.myForm = this.formBuilder.group({
-      usuario: [ Validators.required, Validators.minLength(minPasswordLength)],
-      password: [Validators.required, Validators.minLength(minPasswordLength)],
-      email : [Validators.required, Validators.email]
-    })
-
-  }
-
-
-
+buildForm(){
+  this.myForm = this.formBuilder.group({
+    username: [],
+    password: [],
+    email : []
+  })
 }
+
+
+  //   const minPasswordLength = 8
+    
+  //   this.myForm = this.formBuilder.group({
+  //     usuario: [ Validators.required, Validators.minLength(minPasswordLength)],
+  //     password: [Validators.required, Validators.minLength(minPasswordLength)],
+  //     email : [Validators.required, Validators.email]
+  //   })
+
+  // }
+
+
+  registrarUsuario(form:FormGroup){
+    this.apiService.getUsuario(form.value.username, form.value.email).subscribe((data:any)=>
+    {
+      console.log(data)
+      if(data.type == -1 || data.type == -2){
+        this.respuestaNegativa = "El usuario o el e-mail ya existen"
+      }else{
+        this.apiService.postUsuario(data).subscribe((data:any)=>
+        {
+          if(data.affectedrows=0){
+            this.respuestaNegativa = "El usuario o el e-mail ya existen"
+          }else{
+            console.log(data);
+            this.respuestaPositiva = "La cuenta ha sido creada"
+          }
+      })
+    }
+  })
+}}
