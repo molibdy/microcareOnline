411605a268b72aa165a747412b4e2555b1028feb
molibdy <molipotts@gmail.com>
2021-04-16 15:37:46 +0200

    mm

--- src/app/pages/buscador/barra-buscador/barra-buscador.component.ts
+++ src/app/pages/buscador/barra-buscador/barra-buscador.component.ts
@@ -10,8 +10,8 @@ import { RecetasService } from 'src/app/shared/recetas.service';
 })
 export class BarraBuscadorComponent implements OnInit {
 
-public micronutrientes: []
-public recetas: []
+public micronutrientes: any[]
+public recetas: any[]
 
 
   public inputSearch:string = ""
@@ -30,25 +30,26 @@ public recetas: []
 
    }
 
-   filtrar() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
+  //  filtrar(input:htmlli) {
+  //   // Declare variables
+  //   let input, filter, ul, li, a, i, txtValue;
+  //   input = document.getElementById('myInput');
+  //   filter = input.value.toUpperCase();
+  //   ul = document.getElementById("myUL");
+  //   li = ul.getElementsByTagName('li');
   
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
+  //   // Loop through all list items, and hide those who don't match the search query
+  //   for (i = 0; i < li.length; i++) {
+  //     a = li[i].getElementsByTagName("a")[0];
+  //     txtValue = a.textContent || a.innerText;
+  //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  //       li[i].style.display = "";
+  //     } else {
+  //       li[i].style.display = "none";
+  //     }
+
+  //   }
+  // }
 
 
   ngOnInit(): void {
