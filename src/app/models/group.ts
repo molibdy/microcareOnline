aca2cdfffecaf5637756b754c8dc945a8d8b6e9a
molibdy <molipotts@gmail.com>
2021-04-18 18:48:31 +0200

    merge conflict solved

--- src/app/models/group.ts
+++ src/app/models/group.ts
@@ -1,18 +1,9 @@
 export class Group {
-<<<<<<< HEAD
-    public group_id:number =0
-    public name:string='';
-    public color:string='';
-    public color2:string='';
-    public percent:number=0;
-    public description:string='';
-=======
     public group_id:number;
     public name:string;
     public color:string;
     public color2:string;
     public description:string;
->>>>>>> moli
     constructor(){
    
     }
